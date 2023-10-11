585105ad01f09f41bfa71baff2707e4550bc1544
itsfarhankhan28 <famannat@gmail.com>
2023-10-12 02:15:47 +0530

    feat(script): added new logic to show recipe by area

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
+const searchBtnIngredient = document.getElementById('search-btn-ingredient')
 
 // event listeners
-searchBtn.addEventListener('click', getMealList);
+searchBtn.addEventListener('click',()=> getMealList("i"));
+searchBtnIngredient.addEventListener('click',()=> getMealList("a"))
 mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
     mealDetailsContent.parentElement.classList.remove('showRecipe');
@@ -12,9 +14,9 @@ recipeCloseBtn.addEventListener('click', () => {
 
 
 // get meal list that matches with the ingredients
-function getMealList(){
+function getMealList(searchtype){
     let searchInputTxt = document.getElementById('search-input').value.trim();
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?${searchtype}=${searchInputTxt}`)
     .then(response => response.json())
     .then(data => {
         let html = "";
